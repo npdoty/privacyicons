65a5b878ee7ebdae3ac5ffd3105f928866ce8a2d
Nick Doty <npdoty@ischool.berkeley.edu>
2014-02-09 14:28:51 -0800

    render solely from the acme.json, copying in the site/url for the live tab

--- privacyicons-extension/data/render.js
+++ privacyicons-extension/data/render.js
@@ -14,20 +14,7 @@ self.on("message", function(message) {
 function render() {
   var source = $("#template-holder").html();
   var template = Handlebars.compile(source);
-  var datarows = [
-    {
-      "data-type": "contact information", 
-      "uses": {"service":"yes", "marketing": "opt-out", "profiling": "opt-out"}, 
-      "shares": {"partners":"yes", "public": "opt-in"}
-     },
-     {
-       "data-type": "cookies", 
-       "uses": {"service":"yes", "marketing": "no", "profiling": "opt-out"}, 
-       "shares": {"partners":"opt-out", "public": "no"}
-      }
-  ];    
-  var context = {"datarows": datarows, "site":title, "url": url};
-  var html = template(privacy_data);
-  console.log(html);
+  var context = $.extend(privacy_data, {"site":title, "url": url});
+  var html = template(context);
   $('#chart').html(html);
 }
