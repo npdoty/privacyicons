1902f1e88ef19c890635ad1c63a0b2085b322586
Nick Doty <npdoty@ischool.berkeley.edu>
2014-02-09 15:04:52 -0800

    in progress attempt to download and parse p3p policies where present

--- privacyicons-extension/lib/main.js
+++ privacyicons-extension/lib/main.js
@@ -1,6 +1,7 @@
 var widgets = require("sdk/widget");
 var tabs = require("sdk/tabs");
 var data = require("sdk/self").data;
+var Request = require("sdk/request").Request;
 
 var icon_panel = require("sdk/panel").Panel({
   width: 600,
@@ -24,4 +25,28 @@ var widget = widgets.Widget({
 tabs.on("ready", function(tab){
   console.log("tab ready");
   icon_panel.postMessage({"title": tab.title, "url": tab.url});
+  
+  var p3pRequest = Request({
+    url: tab.url + "/w3c/p3p.xml",
+    onComplete: function(response) {
+      console.log(response.text);
+
+      var re = new RegExp("<POLICY-REF about=\"(.+)\"");
+      var matches = response.text.match(re);
+      // var policyRefs = dom.getElementsByTagName("POLICYREF");
+      // var ref = policyRefs[0].attributes['about'].value;
+      console.log(matches[1]);
+      var p3pSubrequest = Request({
+        url: matches[1],
+        onComplete: function(response) {
+          console.log(response.text);
+          var categoriesRe = new RegExp("<CATEGORIES>\s+<(.+?)\/>", "m");
+          var categoryMatches = response.text.match(categoriesRe);
+          console.log("length: " + categoryMatches.length);
+        }
+      });
+      p3pSubrequest.get();
+    }
+  });
+  p3pRequest.get();
 });
