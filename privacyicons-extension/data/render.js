615f11a83165357d62c0d593b88780cf4339f3a6
Nick Doty <npdoty@ischool.berkeley.edu>
2014-02-09 16:13:53 -0800

    gather whether data is shared publicly or with partners as noted in the site's P3P policy

--- privacyicons-extension/data/render.js
+++ privacyicons-extension/data/render.js
@@ -4,16 +4,42 @@ $(function() {
 
 title = "[title]";  //default title, soon to be replaced
 url = "";
+matches = [];
 
 self.on("message", function(message) {
-  title = message.title;
-  url = message.url;
+  console.log("message received!");
+  if (message.title) {
+    console.log("title message!");
+    title = message.title;
+    url = message.url;
+  } else if (message.matches) {
+    console.log("matches message!");
+    for (var i=0; i<message.matches.length; i++) {
+      console.log(message.matches[i])
+      matches.push({
+        "name": message.matches[i],
+        "share":
+        [
+        {
+          "policy": message.partners ? "yes" : "no"
+        },
+        {
+          "policy": message.public ? "yes" : "no"          
+        }
+        ]
+      });
+    }
+  }
   render();
 });
 
 function render() {
   var source = $("#template-holder").html();
   var template = Handlebars.compile(source);
+  console.log("length: " + matches.length);
+  if (matches.length > 0) {
+    privacy_data['types'] = $.extend(privacy_data.types, matches)
+  }
   var context = $.extend(privacy_data, {"site":title, "url": url});
   var html = template(context);
   $('#chart').html(html);
--- privacyicons-extension/lib/main.js
+++ privacyicons-extension/lib/main.js
@@ -3,6 +3,20 @@ var tabs = require("sdk/tabs");
 var data = require("sdk/self").data;
 var Request = require("sdk/request").Request;
 
+// sample function from: https://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
+function getLocation(href) {
+    var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
+    return match && {
+        protocol: match[1],
+        host: match[2],
+        hostname: match[3],
+        port: match[4],
+        pathname: match[5],
+        search: match[6],
+        hash: match[7]
+    }
+}
+
 var icon_panel = require("sdk/panel").Panel({
   width: 600,
   height: 500,
@@ -23,11 +37,14 @@ var widget = widgets.Widget({
 });
 
 tabs.on("ready", function(tab){
-  console.log("tab ready");
   icon_panel.postMessage({"title": tab.title, "url": tab.url});
+    
+  getLocation(tab.url)['host'];
+  var url = getLocation(tab.url)['protocol'] + "//" + getLocation(tab.url)['host'] + "/w3c/p3p.xml";
+  var host = getLocation(tab.url)['protocol'] + "//" + getLocation(tab.url)['host'];
   
   var p3pRequest = Request({
-    url: tab.url + "/w3c/p3p.xml",
+    url: url,
     onComplete: function(response) {
       console.log(response.text);
 
@@ -36,13 +53,32 @@ tabs.on("ready", function(tab){
       // var policyRefs = dom.getElementsByTagName("POLICYREF");
       // var ref = policyRefs[0].attributes['about'].value;
       console.log(matches[1]);
+      var newUrl = "";
+      if (getLocation(matches[1])) {
+        newUrl = matches[1];
+      } else {
+        newUrl = host + matches[1];
+      }
+      
       var p3pSubrequest = Request({
-        url: matches[1],
+        url: newUrl,
         onComplete: function(response) {
           console.log(response.text);
-          var categoriesRe = new RegExp("<CATEGORIES>\s+<(.+?)\/>", "m");
+          var categoriesRe = new RegExp("<CATEGORIES>\\s*<(.+?)\\/>", "g");
           var categoryMatches = response.text.match(categoriesRe);
-          console.log("length: " + categoryMatches.length);
+          var trimMatches = [];
+          for (var i=0; i < categoryMatches.length; i++) {
+            var match = categoryMatches[i].split("<CATEGORIES>")[1].split("<")[1].split("/")[0].trim();
+            trimMatches.push(match);
+            console.log(match);
+          }
+          
+          var oursRe = new RegExp("<ours\\/>", "g");
+          var ours = response.text.match(oursRe) != null;
+          var publicRe = new RegExp("<public\\/>", "g");
+          var publicFora = response.text.match(publicRe) != null;
+          
+          icon_panel.postMessage({"matches": trimMatches, "partners": ours, "public": publicFora});
         }
       });
       p3pSubrequest.get();
